292ed0bf7e41f4c5ded586d6fc66efff7762c8ae
Linus Molteno <linus@molteno.net>
2021-03-27 17:59:36 +1300

    Fixed bug with gathering of subjects

--- scrape/code/ncea_scraper.py
+++ scrape/code/ncea_scraper.py
@@ -1,10 +1,43 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # scraping library
 import json
-import time
-from datetime import datetime,timedelta
-import random
-import os
+import time 
+from datetime import datetime,timedelta # self explanatory
+import random # for random delays
+import os # for environment variables and filename chekcing
+from unidecode import unidecode # for removing the macrons
+
+# OUTLIERS (before addressing this, replace 
+# macrons with unaccented characters)
+# the lowercase is for matching in case i
+# mis-case something while writing out the LUT
+outliers_lut = {
+    "agribusiness (business studies)": "Business Studies",
+    "mathematics and statistics": "Mathematics",
+    "nga toi": ["Nga Mahi a Te Rehia", # Leisure ?
+                 "Toi Puoro", # Musical Arts
+                 "Toi Ataata"], # Visual Arts
+    "te reo maori": "Reo Maori",
+    "technology": ["Construction and Mechanical Technologies",
+                    "Generic Technology",
+                    "Materials Technology",
+                    "Process Technology",
+                    "Processing Technologies",
+                    "Technology - General Education"],
+    "tikanga-a-iwi": "Tikanga a Iwi",
+    ## WEIRD POLYTECHY ONES TO IGNORE
+    "business & management": False,
+    "core skills": False,
+    "driver license (class 1)": False, # this could be changed later, as there are actually standards in a weird format, i just cant be bothered rn
+    "early childhood education": False,
+    "english for academic purposes": False,
+    "english language (el)": False,
+    "field maori": False,
+    "literacy": False,
+    "numeracy": False,
+    "pacific studies": False,
+    "supported learning": False
+};
 
 of = "../output/ncea_standards.json"
 f_string = "%H:%M:%S %d/%m/%Y"
@@ -16,20 +49,30 @@ def get_subjects(): # this function will parse the NCEA subjects page to find th
     
     page = requests.get(url) # send request
     soup = BeautifulSoup(page.content, 'html.parser') # html/xml parser init
-    results = soup.find_all("li") # find all list items
+    results = soup.find_all("table") # find all tables
+    results = results[0].find_all("li") # find all list items in the first table
     
     subjects = [] # empty list will be populated with subjects
     
     for item in results: # for all the list items
-        if 'levels' in item.a['href']: # if the subject is what we would generally call a subject (e.g. literacy doesn't count)
-            url_ref = item.a['href'].split('/')[3] # find the url name
-            subjects.append({"name": item.a.text, "url": url_ref}) # add to the subjects list
+        # this next line was bad, not good.
+        # if 'levels' in item.a['href']: # if the subject is what we would generally call a subject (if they link normally)
+        subject_name = unidecode(item.a.text) # this is an accent-remover
+        if subject_name.lower() in outliers_lut.keys(): # check if the subject is in the LUT
+            if outliers_lut[subject_name.lower()]: # if it isn't one we should ignore
+                if type(outliers_lut[subject_name.lower()]) == list: # if there are multiple sub-subjects
+                    for subject in outliers_lut[subject_name.lower()]:
+                        subjects.append({"name": subject})
+                else: # there's only one subject
+                    subject_name = outliers_lut[subject_name.lower()]
+            
+        subjects.append({"name": subject_name}) # add to the subjects list
             
     return subjects
 
 def get_assessments(subject): # this function will parse the assessment search query for each level of the given subject
     url = "https://www.nzqa.govt.nz/ncea/assessment/search.do?query={subjname}&view=all&level={level:02d}" # url format
-    assessments = [] # empty list that will be populated with assessments
+    assessments = [] # empty list that will be populated with standards
     
     subjectname = subject['name'].replace(" ", "+") # e.g. turn Digital Technologies into Digital+Technologies for the query
     
@@ -71,6 +114,9 @@ def scrape_and_dump(of):
         json.dump(data, outfile) # write to file
 
 if __name__ == "__main__": # if the module isn't imported
+    print(get_subjects())
+    
+def irrelevant():
     if os.path.isfile(of): # check if the output file exists
         with open(of) as outfile:
             data = json.load(outfile)
