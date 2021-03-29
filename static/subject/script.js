57185d1bdb58dffea5ea44e93f41eacb3d52abc4
Linus Molteno <linus@molteno.net>
2021-03-30 11:13:40 +1300

    Optimisations for index generation

--- static/subject/script.js
+++ static/subject/script.js
@@ -14,6 +14,10 @@ const client = new MeiliSearch({
     apiKey: '',
 })
 
+// subjects stuff
+const subjindex = client.index('subjects')
+const standindex = client.index('standards')
+
 // const for svg icons
 const starOutline = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
   <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
@@ -53,9 +57,6 @@ async function search() {
     searchtext = $("#searchbox").val()
     
     if (searchtext.length != 0) {
-        // standards stuff
-        const standindex = client.index('standards')
-        
         const standards = await standindex.search(searchtext, {limit: 5})
         
         outhtml = '<div class="table-responsive">'
@@ -79,10 +80,7 @@ async function search() {
             });
             outhtml += "</tbody></table>"
         }
-        
-        // subjects stuff
-        const subjindex = client.index('subjects')
-        
+
         const subjects = await subjindex.search(searchtext, {limit: 5})
         if (subjects.hits.length > 0) {
             outhtml += `<h3 class="mb-1">Subjects</h3>
